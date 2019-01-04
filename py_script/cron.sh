607a9363599ed27136f0bcbb275b680e69c3725d
Chipz <chipz.ciptadi@live.com>
2019-01-04 12:05:51 +0700

    update path

--- py_script/cron.sh
+++ py_script/cron.sh
@@ -1,4 +1,4 @@
 # executed every 15 minutes
-python3 /var/www/socialmedia/py_script/create_normalize_dict.py
-python3 /var/www/socialmedia/py_script/get_tweets.py
-python3 /var/www/socialmedia/py_script/tweet_similarity.py
+python3 /var/www/thesis-socialmedia/py_script/create_normalize_dict.py
+python3 /var/www/thesis-socialmedia/py_script/get_tweets.py
+python3 /var/www/thesis-socialmedia/py_script/tweet_similarity.py
