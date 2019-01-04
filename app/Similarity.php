1ec3836a71bb29057694d19ea9182afc5c3bef1a
Chipz <chipz.ciptadi@live.com>
2019-01-04 20:27:07 +0700

    add relationship

--- app/Similarity.php
+++ app/Similarity.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Similarity extends Model
 {
-    //
+    public function tweet()
+    {
+        return $this->belongsTo('App\Tweet', 'first_tweet_id', 'tweet_id');
+    }
 }
