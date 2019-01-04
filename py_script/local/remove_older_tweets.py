42bff6c0f900dcc7543ded8794d1f30258297149
Chipz <chipz.ciptadi@live.com>
2019-01-04 11:34:56 +0700

    Added local script file

--- py_script/local/remove_older_tweets.py
+++ py_script/local/remove_older_tweets.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pymysql
 
-connection = pymysql.connect(host='localhost', user='nurul', password='nurul', db='socialmedia', charset='utf8')
+connection = pymysql.connect(user="root", password="", host="127.0.0.1", database="nurul", charset='utf8')
 
 with connection.cursor() as cursor:
     sql = 'delete from tweets where date(tweet_created_at) < DATE_SUB(CURDATE(), INTERVAL 3 DAY);'
