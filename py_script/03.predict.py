b7ebf28f36cc821e209a616de7267e1db6cbca32
Chipz <chipz.ciptadi@live.com>
2019-01-15 08:38:15 +0700

    add model and prediction script

---
+++ py_script/03.predict.py
@@ -0,0 +1,45 @@
+print('Script started')
+
+
+print('Importing libraries')
+import pickle
+import pandas as pd
+import pymysql
+
+from credentials import DB_HOSTNAME, DB_NAME, DB_USERNAME, DB_PASSWORD
+
+
+print('Loading model.pkl')
+vectorizer, clf = pickle.load('model.pkl')
+
+
+print('Loading tweets with similarity >= 0.5 and < 1')
+connection = pymysql.connect(user=DB_USERNAME, password=DB_PASSWORD, host=DB_HOSTNAME, database=DB_NAME, charset='utf8')
+query = """
+    SELECT `similarities`.batch, tweet_id, full_text_clean
+    FROM `tweets` 
+    JOIN `similarities` ON `tweets`.`tweet_id`=`similarities`.`first_tweet_id` 
+    JOIN (
+        SELECT batch FROM `similarities` ORDER BY batch DESC LIMIT 1
+    ) as last_batch ON last_batch.batch=`similarities`.`batch`
+    WHERE 
+        similarity >= 0.5 AND similarity < 1.0
+"""
+df = pd.read_sql(query, connection)
+
+
+print('Extracting features using TF-IDF')
+X = vectorizer.transform(df.full_text_clean)
+
+
+print('Predicting class')
+y_pred = clf.predict(X)
+df_predicted = pd.concat([df, pd.Series(y_pred)], axis=1, ignore_index=True)
+print(df_predicted)
+
+
+print('Closing connection')
+connection.close()
+
+
+print('Script done')
